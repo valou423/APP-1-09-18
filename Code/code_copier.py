903b5c8abb48124a4d33c9281bc0a4ded1b1f1f2
Alexaucalme <alexandre.monroche@gmail.com>
2018-09-26 11:57:10 +0200

    V1

--- Code/code_copier.py
+++ Code/code_copier.py
@@ -7,11 +7,11 @@ Created on Tue Sep 18 14:36:41 2018
 
 import random
 
-#fonction HanoÃ®
+#fonction Hanoî
 
 def test_2(silo_initial, silo_final):
     if silo_initial != []:
-         print(" le premier conteneur du silot " + str(silo_initial) + " se dÃ©place vers le silot " + str(silo_final))
+         print(" le premier conteneur du silot " + str(silo_initial) + " se déplace vers le silot " + str(silo_final))
          a = silo_initial.pop()
          silo_final.append(a)
 
@@ -25,7 +25,7 @@ def hanoi_2(X, silo_initial, silo_final, silo_intermediaire):
 
 def hanoi(X, silo_initial, silo_final, silo_intermediaire):
     hanoi_2(X, silo_initial, silo_final, silo_intermediaire)
-    print( " Ã©tat du silo final : " + str(silo_final))
+    print( " état du silo final : " + str(silo_final))
 
 #Remplissage du tube initial
 
@@ -41,9 +41,9 @@ temporaire=[]
 for i in range(1,conteneurs+1):
     del tube[-1]
     temporaire.append(i)
-    print("Le conteneur de radioactivitÃ©",i,"a Ã©tÃ© transfÃ©rÃ© du tube au silo temporaire")
+    print("Le conteneur de radioactivité",i,"a été transféré du tube au silo temporaire")
 
-#DÃ©termination du nombre de silo Ã  utiliser, et crÃ©ation de ceux ci sous forme de listes
+#Détermination du nombre de silo à utiliser, et création de ceux ci sous forme de listes
 
 print("\nCombien de silos doit-on utiliser? Le minimum de silo requis est 3")
 nombre_de_silo = int(input())
@@ -54,7 +54,7 @@ silos=[]
 for a in range(1,nombre_de_silo+1):
     silos.append([])
 
-#RÃ©partition alÃ©atoire dans les silos, avec respect de la contrainte
+#Répartition aléatoire dans les silos, avec respect de la contrainte
 
 silo_precedent=0
 essais_de_distribution=conteneurs
@@ -64,46 +64,46 @@ while essais_de_distribution>0:
         silo_precedent=silo_actuel
         silos[silo_actuel-1].append(essais_de_distribution)
         del temporaire[-1]
-        print("Le conteneur de radioactivitÃ©",essais_de_distribution,"a Ã©tÃ© transfÃ©rÃ© dans le silo",silo_actuel)
+        print("Le conteneur de radioactivité",essais_de_distribution,"a été transféré dans le silo",silo_actuel)
         essais_de_distribution-=1
 
-#Recherche de la pile d'arrivÃ©e en fonction de la pile de dÃ©part
+#Recherche de la pile d'arrivée en fonction de la pile de départ
 
 found=False
 silo_compteur=0
 conteneur_cible=2
-dÃ©part=silo_actuel-1
-nombre_a_dÃ©placer =1
+départ=silo_actuel-1
+nombre_a_déplacer =1
 while not found:
     if len(silos[silo_compteur])!=0:
         if silos[silo_compteur][-1]==conteneur_cible:
-            arrivÃ©e=silo_compteur
+            arrivée=silo_compteur
             conteneur_cible+=1
             found = True
         else:
             silo_compteur+=1
     else:
         silo_compteur+=1
-#DÃ©termination d'un silo intÃ©rmÃ©diaire, qui n'est ni le silo de dÃ©part, ni celui d'arrivÃ©e
+#Détermination d'un silo intérmédiaire, qui n'est ni le silo de départ, ni celui d'arrivée
 
 inter=random.randint(1,nombre_de_silo)-1
-while inter == dÃ©part or inter == arrivÃ©e:
+while inter == départ or inter == arrivée:
     inter=random.randint(1,nombre_de_silo)-1
 
-#Algorithme de HanoÃ¯ rÃ©pÃ©tÃ© jusqu'Ã  rÃ©solution
+#Algorithme de Hanoï répété jusqu'à résolution
 
-while nombre_a_dÃ©placer<conteneurs:
-    hanoi(nombre_a_dÃ©placer, silos[dÃ©part], silos[arrivÃ©e], silos[inter])
-    if len(silos[arrivÃ©e])==conteneurs:
+while nombre_a_déplacer<conteneurs:
+    hanoi(nombre_a_déplacer, silos[départ], silos[arrivée], silos[inter])
+    if len(silos[arrivée])==conteneurs:
         break
-    nombre_a_dÃ©placer+=1
-    dÃ©part=arrivÃ©e
+    nombre_a_déplacer+=1
+    départ=arrivée
     silo_compteur=0
     found=False
     while not found:
         if len(silos[silo_compteur])!=0:
             if silos[silo_compteur][-1]==conteneur_cible:
-                arrivÃ©e=silo_compteur
+                arrivée=silo_compteur
                 conteneur_cible+=1
                 found=True
             else:
@@ -111,13 +111,13 @@ while nombre_a_dÃ©placer<conteneurs:
         else:
             silo_compteur+=1
     inter=random.randint(1,nombre_de_silo)-1
-    while inter == dÃ©part or inter == arrivÃ©e:
+    while inter == départ or inter == arrivée:
         inter=random.randint(1,nombre_de_silo)-1
 
-#DÃ©placement du tout dans le NiÃ¨me silo si nÃ©cessaire
+#Déplacement du tout dans le Nième silo si nécessaire
 
-if arrivÃ©e != nombre_de_silo-1:
+if arrivée != nombre_de_silo-1:
     inter=random.randint(1,nombre_de_silo)-1
-    while inter == arrivÃ©e or inter == nombre_de_silo-1:
+    while inter == arrivée or inter == nombre_de_silo-1:
         inter=random.randint(1,nombre_de_silo)-1
-    hanoi(conteneurs,silos[arrivÃ©e],silos[nombre_de_silo-1],silos[inter])
+    hanoi(conteneurs,silos[arrivée],silos[nombre_de_silo-1],silos[inter])
